fa5aeeeec12c64dc8a816dd5180095577d0d3d10
IM_AlirezaYT <125809189+alirezayt@users.noreply.github.com>
2024-03-06 20:47:00 +0330

    Delete ytvision.py

--- ytvision.py
+++
@@ -1,7 +0,0 @@
-import marshal
-with open('my_function.marshal', 'rb') as file:
-    decoded_function = marshal.loads(file.read())
-def new_function():
-    pass
-new_function.__code__ = decoded_function
-new_function()
