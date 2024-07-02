78a34f9c6244bb80b3a8fd2eccc74082fa098a71
IM_AlirezaYT <125809189+alirezayt@users.noreply.github.com>
2024-07-02 23:48:55 +0330

    Update ytvisionpack.py

--- ytvisionpack.py
+++ ytvisionpack.py
@@ -1,7 +1,7 @@
 import importlib.util
 
-module_name = 'file'
-module_path = 'file.pyc'
+module_name = 'pack.cpython-38'  # نام ماژول بدون پسوند
+module_path = 'pack.cpython-38.pyc'  # مسیر فایل pyc
 
 spec = importlib.util.spec_from_file_location(module_name, module_path)
 module = importlib.util.module_from_spec(spec)
