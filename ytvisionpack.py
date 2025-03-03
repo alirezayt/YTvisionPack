8b40546c5c39ccfb84b21029560559860f6c4d65
IM_AlirezaYT <125809189+alirezayt@users.noreply.github.com>
2025-03-03 22:40:25 +0330

    Delete ytvisionpack.py

--- ytvisionpack.py
+++
@@ -1,8 +0,0 @@
-import importlib.util
-
-module_name = 'pack.cpython-38'  # نام ماژول بدون پسوند
-module_path = 'pack.cpython-38.pyc'  # مسیر فایل pyc
-
-spec = importlib.util.spec_from_file_location(module_name, module_path)
-module = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(module)
